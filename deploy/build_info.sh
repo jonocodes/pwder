f2fafe6f994e064947aceeb55674246897dc511e
Jono Finger <jono@foodnotblogs.com>
2017-11-09 11:05:11 -0800

    will this fix travis_branch?

--- deploy/build_info.sh
+++ deploy/build_info.sh
@@ -5,3 +5,6 @@ git_branch: $(git rev-parse --abbrev-ref HEAD)
 git_checksum: $(git rev-parse HEAD)
 build_date: $(date)
 EOF
+
+# HACK: since git_branch above always returns HEAD for every branch on travis
+[ -z $TRAVIS_BRANCH ] || echo travis_branch: $TRAVIS_BRANCH
